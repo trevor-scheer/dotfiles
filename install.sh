a1cbcfecc1d4e79dc996627f340f98d7ca0b33e9
Trevor Scheer <trevor.scheer@gmail.com>
2025-07-26 13:18:21 -0700

    fix install script for volta

--- install.sh
+++ install.sh
@@ -15,6 +15,10 @@ if ! command -v volta &> /dev/null; then
   curl https://get.volta.sh | bash
   echo 'export VOLTA_HOME="$HOME/.volta"' >> $HOME/.zprofile
   echo 'export PATH="$VOLTA_HOME/bin:$PATH"' >> $HOME/.zprofile
+  
+  # Source the new environment to make volta available immediately
+  export VOLTA_HOME="$HOME/.volta"
+  export PATH="$VOLTA_HOME/bin:$PATH"
 else 
   echo "✅ Volta is already installed."
 fi
