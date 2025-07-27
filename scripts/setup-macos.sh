f09f58354feceb087bc6de55126c461fb6a46766
Trevor Scheer <trevor.scheer@gmail.com>
2025-07-27 10:00:07 -0700

    xcode

--- scripts/setup-macos.sh
+++ scripts/setup-macos.sh
@@ -2,7 +2,13 @@
 
 # macOS specific setup
 
-
+# Install Xcode command line tools
+if xcode-select -p &> /dev/null; then
+    echo "✅ Xcode Command Line Tools are already installed."
+else
+    echo "💡 Installing Xcode Command Line Tools..."
+    xcode-select --install || true
+fi
 
 # Additional macOS specific configurations can be added here
 
