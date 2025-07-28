3f360d1813f4099da0baedd278f17dcc99b396d4
Trevor Scheer <trevor.scheer@gmail.com>
2025-07-28 09:26:02 -0700

    small fixes, readd codespace +cursor helpers

--- config/shell/utilities/vanta.sh
+++ config/shell/utilities/vanta.sh
@@ -1,15 +1,46 @@
 alias jdsw="just dev-start-web"
 alias mdsw="jdsw"
+
+csssh() {
+  temp="___setupCodespaceSSH.temp.js"
+  gh api repos/VantaInc/obsidian/contents/scripts/setupCodespaceSSH.js \
+    --jq '.content' | base64 -d > "$temp"
+  node "$temp"
+  rm "$temp"
+}
+
+newcs() {
+  cs_id=$(gh cs create -R VantaInc/obsidian)
+  echo "Codespace created, waiting for it to be ready...(~40s arbitrarily)"
+  sleep 40
+  echo "Codespace ready, setting up SSH..."
+  
+  csssh
+  
+  cursor -n --folder-uri "vscode-remote://ssh-remote+${cs_id}.github.dev/workspaces/obsidian"
+}
+
+opencs() {
+  echo "TODO"
+}
+
+stopcs() {
+  temp="___shutdownCodespaces.temp.js"
+  gh api repos/VantaInc/obsidian/contents/scripts/shutdownCodespaces.cjs \
+    --jq '.content' | base64 -d > "$temp"
+  node "$temp"
+  rm "$temp"
+}
+
+turboclient() {
+  turbo -F @vanta/web-client "$@"
+}
+alias tc=turboclient
+
 alias dr="make dev-replace"
 alias drw="make dev-replace web-client"
 alias drs="docker container restart obsidian-web-client.internal-1"
 alias logs="make dev-watch-logs"
 alias yww="yarn workspace @vanta/web-client"
-
-alias tc=turboclient
-
-turboclient() {
-  turbo -F @vanta/web-client "$@"
-}
 
 echo "✅ Vanta utilities loaded."
