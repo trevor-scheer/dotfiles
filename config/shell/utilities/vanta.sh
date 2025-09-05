ff9d8e339ea8b49b1deb5cba792664c4d6b95729
Trevor Scheer <trevor.scheer@gmail.com>
2025-09-05 15:04:03 -0700

    some vanta improvements

--- config/shell/utilities/vanta.sh
+++ config/shell/utilities/vanta.sh
@@ -47,16 +47,22 @@ stopcs() {
 turboclient() {
   turbo -F @vanta/web-client "$@"
 }
+
+yarnweb() {
+  yarn workspace @vanta/web-client "$@"
+}
 alias tc=turboclient
 
-alias dr="make dev-replace"
-alias drw="make dev-replace web-client"
+alias dr="just dev-replace"
+alias drw="just dev-replace web-client"
 alias drs="docker container restart obsidian-web-client.internal-1"
-alias logs="make dev-watch-logs"
-alias yww="yarn workspace @vanta/web-client"
+alias logs="just dev-watch-logs"
+alias jpp="just post-pull"
+alias yww="yarnweb"
 
 function codemod() {
-  ./build-tools/vsx.sh scripts/refactoring-tools/src/gql/gql-tada-codemod.ts "$@"
+  # ./build-tools/vsx.sh scripts/refactoring-tools/src/gql/gql-tada-codemod.ts "$@"
+  node ./parcel-plugins/parcel-transformer-gqlts/bin/index.js "**/src/**/*.(ts|tsx)"
 }
 
 echo "✅ Vanta utilities loaded."
